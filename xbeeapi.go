85d6ce8708dc80c51229beab86d559c9adcbece5
coreyshuman <ctshumancode@gmail.com>
2016-05-28 05:24:20 -0700

    built out the 'sned packet' method

--- xbeeapi.go
+++ xbeeapi.go
@@ -10,10 +10,10 @@ package xbeeapi
 import (
 	"github.com/coreyshuman/serial"
 	"github.com/coreyshuman/srbuf"
-	"time"
+	//"time"
 	"fmt"
-	"bufio"
-	"bytes"
+	//"bufio"
+	//"bytes"
 	"errors"
 	"container/list"
 )
@@ -25,14 +25,14 @@ type RxHandlerFunc func(string)
 type RxHandler struct {
 	id int
 	name string
-	RxHandlerFunc func
+	// RxHandlerFunc func
 }
 
 var rxHandlerList *list.List
-quit := make(chan bool)
+var quit chan bool 
 
-var txBuf srbuf.SimpleRingBuff
-var rxBuf srbuf.SimpleRingBuff
+var txBuf *srbuf.SimpleRingBuff
+var rxBuf *srbuf.SimpleRingBuff
 
 var serialXBEE int = -1
 var err error
@@ -45,8 +45,10 @@ func Init(dev string, baud int, timeout int) {
 	rxBuf = srbuf.Create(256)
 	// initialize a serial interface to the xbee module
 	serialXBEE, err = serial.Connect(dev, baud, timeout)
+	quit = make(chan bool)
 }
 
+/*
 func Begin() {
 	if serialXBEE == -1 {
 		return
@@ -58,20 +60,24 @@ func Begin() {
 			case <- quit:
 				return
 			default:
-				
+				return // cts debug
 			}
 		}
 		// if we get here, dispose and exit
 		serial.Disconnect(serialXBEE)
-	}
+	}()
 }
-
+*/
+/*
 func End() {
 	quit <- true
 }
 
 func processRxData()
 {
+	if true {
+	
+	}
 	// use rxBuf to parse out data
 }
 
@@ -84,19 +90,58 @@ func processTxData()
 	}
 }
 
-func SendPacket(address byte[], data byte[], length int) {
+/* ***************************************************************
+ * Send data packet as an RF packet to the specified destination
+ *
+ * 0		- Start Delimiter
+ * 1-2		- Length
+ * 3		- Frame Type (0x10)
+ * 4		- Frame ID 
+ * 5 - 12	- 64-bit address MSB-LSB
+ * 13 - 14	- 16-bit address MSB-LSB
+ * 15		- broadcast radius
+ * 16		- options
+ * 17 - n	- RF data payload
+ * n+1		- checksum
+ * ***************************************************************/
+func SendPacket(address64 []byte, address16 []byte, option byte, data []byte) (d []byte, n int, err error) {
+	d = []byte{0x7E, 0x00, 0x00, 0x10, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0xFF, 0xFE, 0x00, 0x00}
 	
-	// do a bunch of stuff
 	
-	err := txBuffer.WriteByte(byte('c'))
-	if err != nil {
-		fmt.Print(err)
+	if len(address64) != 8 {
+		return d, 0, errors.New("Incorrect Address Length")
 	}
+	
+	// 64-bit address
+	copy(d[5:13], address64)
+	
+	if address16 != nil && len(address16) == 2 {
+		copy(d[13:15], address16)
+	}
+	
+	d[16] = option
+	
+	d = append(d[:], data[:]...)
+	d = append(d[:], 0x00)
+	
+	n = len(d)
+	d[1] = byte((n-4) / 0x100)
+	d[2] = byte((n-4) % 0x100)
+	
+	d[n-1] = CalcChecksum(d[3:])
+	fmt.Println(string(d))
+	return
 }
 
-func CalcChecksum()
-{
-
+func CalcChecksum(data []byte)(byte) {
+	n := len(data)
+	var cs byte = 0
+	
+	for i := 0; i < n-1; i++ {
+		cs += data[i]
+	}
+	return 0xFF - cs
 }
 
 
