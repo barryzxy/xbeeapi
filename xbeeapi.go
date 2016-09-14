3dcad70f3bcb3e99eb84ef8fe2477f00889412c1
coreyshuman <ctshumancode@gmail.com>
2016-09-14 02:43:33 -0700

    Added more API support and fixed some issues

--- xbeeapi.go
+++ xbeeapi.go
@@ -19,6 +19,15 @@ import (
 	"encoding/hex"
 )
 
+const ATCOMMAND		= 0x08
+const TRANSMIT		= 0x10
+const ATRESPONSE	= 0x88
+const MODEMSTATUS	= 0x8A
+const TXSTATUS		= 0x8B
+const RECEIVE		= 0x90
+const EXPLICITRX	= 0x91
+
+
 // receive handler signature
 type RxHandlerFunc func([]byte)
 
@@ -110,7 +119,9 @@ func SetupErrorHandler(f func(error)) {
 
 func processRxData() {
 	var ret bool = false
+	var frameType byte
 	var frameId byte
+	var status byte
 	var err error
 	var d []byte
 	var n int
@@ -143,14 +154,20 @@ func processRxData() {
 		ret = true
 		// if we get here, packet is ready to parse
 		data := rxBuf.GetBytes(n+4)
-		switch(data[3]) { // Frame Type
-			case 0x88 : // at command response
+		frameType = data[3]
+		switch frameType { // Frame Type
+			case ATRESPONSE : 
 				frameId, data, err = ParseATCommandResponse(data)
-				break
-				
+				frameId = frameId
+			case MODEMSTATUS : 
+				status, err = ParseModemStatusResponse(data)
+				if err == nil {
+					data = []byte(GetModemStatusDescription(status))
+				}	
+			case TXSTATUS : 
+				_, _, _, data, err = ParseReceivePacketResponse(data)
 			default:
 				err = errors.New("Frame Type not supported: [" + hex.Dump(data[3:4]) + "]")
-				break
 		}
 		if(err != nil) {
 			if(errHandler != nil) {
@@ -159,7 +176,7 @@ func processRxData() {
 			return
 		}
 		// fire callback
-		handler := findHandler(frameId) 
+		handler := findHandler(frameType) 
 		if(handler != nil) {
 			handler(data)
 		} else {
@@ -306,11 +323,11 @@ func ParseATCommandResponse(r []byte) (frameId byte, data []byte, err error) {
 	
 	check := CalcChecksum(r[3:n+3])
 	if(check != r[n+3]) {
-		return 0, nil, errors.New(fmt.Sprintf( "Checksum Error: calc=[%02X] read=[%02X]", check, r[n+3] ) )
+		return 0, nil, errors.New(fmt.Sprintf( "Checksum Error: calc=[%02X] read=[%02X]", check, r[n+3]))
 	}
 	
 	// prepare return data
-	frameId = r[3]
+	frameId = r[4]
 
 	if(n > 5) {
 		data = r[8:3+n] // 8:8+n-5
@@ -321,6 +338,350 @@ func ParseATCommandResponse(r []byte) (frameId byte, data []byte, err error) {
 	return
 }
 
+/* ***************************************************************
+ * ParseModemStatusResponse
+ * Parse a modem RF module status message
+ *
+ * 0		- Start Delimiter
+ * 1-2		- Length
+ * 3		- Frame Type (0x8A)
+ * 4		- Status
+ * 5		- checksum
+ * ***************************************************************/
+func ParseModemStatusResponse(r []byte) (status byte, err error) {
+	status = 0
+	err = nil
+	if(r[3] != 0x8A) {
+		err = errors.New("Invalid Frame Type") 
+		return
+	}
+	
+	n := int(r[1])*256 + int(r[2])
+	
+	if(n != len(r) - 4) {
+		err = errors.New("Frame Length Error: " + fmt.Sprintf("%d, %d", n, len(r)-4)) 
+		return
+	}
+	
+	check := CalcChecksum(r[3:n+3])
+	if(check != r[n+3]) {
+		err = errors.New(fmt.Sprintf( "Checksum Error: calc=[%02X] read=[%02X]", check, r[n+3]))
+		return
+	}
+	
+	// prepare return data
+	status = r[4]
+	return
+}
 
+/* ***************************************************************
+ * GetModemStatusDescription
+ * Convert modem status code to description string
+ *
+ * Input:
+ *			status byte
+ * Output:
+ *			description string
+ *
+ * ***************************************************************/
+func GetModemStatusDescription(status byte) (description string) {
+	switch {
+		case status == 0:
+			return "Hardware reset"
+		case status == 1:
+			return "Watchdog timer reset"
+		case status == 2:
+			return "Joined network"
+		case status == 3:
+			return "Disassociated"
+		case status == 6:
+			return "Coordinator started"
+		case status == 7:
+			return "Network security key updated"
+		case status == 0x0d:
+			return "Voltage supply limit exceeded"
+		case status == 0x11:
+			return "Modem configuration changed while join in progress"
+		case status >= 0x80:
+			return "Stack error"
+	}
+	return "Unknown status"
+}
 
+/* ***************************************************************
+ * ParseTransmitStatusResponse
+ * Parse the TX request transmit status message.
+ *
+ * 0		- Start Delimiter
+ * 1-2		- Length
+ * 3		- Frame Type (0x8B)
+ * 4		- Frame ID 
+ * 5 - 6	- 16-bit address of destination
+ * 7		- Transmit Retry Count
+ * 8		- Delivery Status
+ * 9		- Discovery Status
+ * 10		- checksum
+ * ***************************************************************/
+func ParseTransmitStatusResponse(r []byte) (frameId byte, destinationAddress [2]byte, retryCount byte, deliveryStatus byte, discoveryStatus byte, err error) {
+	frameId = 0
+	retryCount = 0
+	deliveryStatus = 0
+	discoveryStatus = 0
+	err = nil
+	
+	if(r[3] != 0x8B) {
+		err = errors.New("Invalid Frame Type") 
+		return
+	}
+	
+	n := int(r[1])*256 + int(r[2])
+	
+	if(n != len(r) - 4) {
+		err = errors.New("Frame Length Error: " + fmt.Sprintf("%d, %d", n, len(r)-4)) 
+		return
+	}
+	
+	check := CalcChecksum(r[3:n+3])
+	if(check != r[n+3]) {
+		err = errors.New(fmt.Sprintf( "Checksum Error: calc=[%02X] read=[%02X]", check, r[n+3]))
+		return
+	}
+	
+	// prepare return data
+	frameId = r[4]
+	copy(destinationAddress[:], r[5:7])
+	retryCount = r[7]
+	deliveryStatus = r[8]
+	discoveryStatus = r[9]
+	
+	return
+}
 
+/* ***************************************************************
+ * GetDeliveryStatusDescription
+ * Convert transmit delivery status code to description string
+ *
+ * Input:
+ *			status byte
+ * Output:
+ *			description string
+ *
+ * ***************************************************************/
+func GetDeliveryStatusDescription(status byte) (description string) {
+	switch status {
+		case 0x00:
+			return "Success"
+		case 0x01:
+			return "MAC ACK failure"
+		case 0x02:
+			return "CCA failure"
+		case 0x15:
+			return "Invalid destination endpoint"
+		case 0x21:
+			return "Network ACK failure"
+		case 0x22:
+			return "Not joined to network"
+		case 0x23:
+			return "Self-addressed"
+		case 0x24:
+			return "Address not found"
+		case 0x25:
+			return "Route not found"
+		case 0x26:
+			return "Broadcast source failed to hear a neighbor relay the message"
+		case 0x2b:
+			return "Invalid binding table index"
+		case 0x2c:
+			return "Resource error (lack of free buffers, timers, etc)"
+		case 0x2d:
+			return "Attempted broadcast with APS transmission"
+		case 0x2e:
+			return "Attempted unicast with APS transmission, but EE=0"
+		case 0x32:
+			return "Resource error (lack of free buffers, timers, etc)"
+		case 0x74:
+			return "Data payload too large"
+		case 0x75:
+			return "Indirect message unrequested"
+	}
+	return "Unknown status"
+}
+
+/* ***************************************************************
+ * GetDiscoveryStatusDescription
+ * Convert transmit discovery status code to description string
+ *
+ * Input:
+ *			status byte
+ * Output:
+ *			description string
+ *
+ * ***************************************************************/
+func GetDiscoveryStatusDescription(status byte) (description string) {
+	switch status {
+		case 0x00:
+			return "No discovery overhead"
+		case 0x01:
+			return "Address discovery"
+		case 0x02:
+			return "Route discovery"
+		case 0x03:
+			return "Address and route"
+		case 0x40:
+			return "Extended timeout discovery"	
+	}
+	return "Unknown status"
+}
+
+/* ***************************************************************
+ * ParseReceivePacketResponse
+ * Parse the RF data received packet
+ *
+ * 0		- Start Delimiter
+ * 1-2		- Length
+ * 3		- Frame Type (0x90)
+ * 4 - 11   - 64-bit address of sender (MSB - LSB)
+ * 12 - 13	- 16-bit address of sender
+ * 14		- Receive options
+ * 15 - n	- Received data
+ * n+1		- checksum
+ * ***************************************************************/
+func ParseReceivePacketResponse(r []byte) (destinationAddress64 [8]byte, destinationAddress16 [2]byte, receiveOptions byte, receivedData []byte, err error) {
+	receiveOptions = 0
+	err = nil
+	
+	if(r[3] != 0x90) {
+		err = errors.New("Invalid Frame Type") 
+		return
+	}
+	
+	n := int(r[1])*256 + int(r[2])
+	
+	if(n != len(r) - 4) {
+		err = errors.New("Frame Length Error: " + fmt.Sprintf("%d, %d", n, len(r)-4)) 
+		return
+	}
+	
+	check := CalcChecksum(r[3:n+3])
+	if(check != r[n+3]) {
+		err = errors.New(fmt.Sprintf( "Checksum Error: calc=[%02X] read=[%02X]", check, r[n+3]))
+		return
+	}
+	
+	// prepare return data
+	copy(destinationAddress64[:], r[4:12])
+	copy(destinationAddress16[:], r[12:14])
+	receiveOptions = r[14]
+	
+	if(n > 12) {
+		receivedData = r[15:3+n] // 15:15+n-12
+	} 
+	
+	return
+}
+
+/* ***************************************************************
+ * GetReceiveOptionDescription
+ * Convert transmit discovery status code to description string
+ *
+ * Input:
+ *			status byte
+ * Output:
+ *			description string
+ *
+ * ***************************************************************/
+func GetReceiveOptionDescription(status byte) (description string) {
+	var d [100]byte
+	i := 0
+	
+	if status & 0x01 > 0 {
+		i += copy(d[i:], []byte("Packet acknowledged."))
+	}
+	
+	if status & 0x02 > 0 {
+		i += copy(d[i:], []byte("Packet was a broadcast packet."))
+	}
+	
+	if status & 0x20 > 0 {
+		if i > 0 {
+			d[i] = ' ';
+			i++
+		}
+		i += copy(d[i:], []byte("Packet encrypted with APS encryption."))
+	}
+	
+	if status & 0x40 > 0 {
+		if i > 0 {
+			d[i] = ' ';
+			i++
+		}
+		i += copy(d[i:], []byte("Packet was sent from an end device."))
+	}
+	
+	if i == 0 {
+		return "Unkown receive options."
+	} else {
+		return string(d[0:i])
+	}
+}
+
+/* ***************************************************************
+ * ParseExplicitReceivePacketResponse
+ * Parse the RF data received packet when explicit mode enabled
+ *
+ * 0		- Start Delimiter
+ * 1-2		- Length
+ * 3		- Frame Type (0x90)
+ * 4 - 11   - 64-bit address of sender (MSB - LSB)
+ * 12 - 13	- 16-bit address of sender
+ * 14		- Source Endpoint
+ * 15		- Destination Endpoint
+ * 16 - 17  - Cluster ID the packet was addressed to
+ * 18 - 19  - Profile ID the packet was addressed to
+ * 20		- Receive options
+ * 21 - n	- Received data
+ * n+1		- checksum
+ * ***************************************************************/
+func ParseExplicitReceivePacketResponse(r []byte) (destinationAddress64 [8]byte, destinationAddress16 [2]byte,
+													sourceEndpoint byte, destinationEndpoint byte,
+													clusterId [2]byte, profileId [2]byte,
+													receiveOptions byte, receivedData []byte, err error) {
+	sourceEndpoint = 0
+	destinationEndpoint = 0
+	receiveOptions = 0
+	receivedData = nil
+	err = nil
+	
+	if(r[3] != 0x91) {
+		err = errors.New("Invalid Frame Type") 
+		return
+	}
+	
+	n := int(r[1])*256 + int(r[2])
+	
+	if(n != len(r) - 4) {
+		err = errors.New("Frame Length Error: " + fmt.Sprintf("%d, %d", n, len(r)-4)) 
+		return
+	}
+	
+	check := CalcChecksum(r[3:n+3])
+	if(check != r[n+3]) {
+		err = errors.New(fmt.Sprintf( "Checksum Error: calc=[%02X] read=[%02X]", check, r[n+3]))
+		return
+	}
+	
+	// prepare return data
+	copy(destinationAddress64[:], r[4:12])
+	copy(destinationAddress16[:], r[12:14])
+	sourceEndpoint = r[14]
+	destinationEndpoint = r[15]
+	copy(clusterId[:], r[16:18])
+	copy(profileId[:], r[18:20])
+	receiveOptions = r[20]
+	
+	if(n > 18) {
+		receivedData = r[21:3+n] // 21:21+n-18
+	} 
+	
+	return
+}
